9e2652191825e7d80d6e60c1d6593130d91dd674
Julia Zibordi <julia.zibordi@gmail.com>
2026-04-04 23:06:04 -0300

    adiciona modelos dos itens de cápsula

--- capsula_tempo/core/admin.py
+++ capsula_tempo/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Capsula, Usuario
+from .models import Capsula, Usuario, ItemTexto, ItemImagem, ItemLink
 
 admin.site.register(Usuario)
 admin.site.register(Capsula)
+admin.site.register(ItemTexto)
+admin.site.register(ItemImagem)
+admin.site.register(ItemLink)
--- capsula_tempo/core/models.py
+++ capsula_tempo/core/models.py
@@ -4,22 +4,32 @@ from django.conf import settings
 
 class Usuario(AbstractUser):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
 class Capsula(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='capsulas')
     titulo = models.CharField(max_length=100)
-    mensagem = models.TextField()
     data_abertura = models.DateTimeField()
-
-    imagem = models.ImageField(upload_to='capsulas/', blank=True, null=True)
-    link = models.URLField(blank=True, null=True)
-    
     criada_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
+    
+class ItemTexto(models.Model):
+    capsula = models.ForeignKey(Capsula, on_delete=models.CASCADE, related_name='textos')
+    criado_em = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField()
+
+class ItemImagem(models.Model):
+    capsula = models.ForeignKey(Capsula, on_delete=models.CASCADE, related_name='imagens')
+    criado_em = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='capsulas/')
+
+class ItemLink(models.Model):
+    capsula = models.ForeignKey(Capsula, on_delete=models.CASCADE, related_name='links')
+    criado_em = models.DateTimeField(auto_now_add=True)
+    link = models.URLField()
 
