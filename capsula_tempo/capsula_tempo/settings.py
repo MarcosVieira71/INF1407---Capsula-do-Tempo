adba7e8248048e9a6b90e4905131dc7c064fd559
MarcosVieira71 <marcosvieira120404@gmail.com>
2026-04-21 11:22:43 -0300

    adiciona whitenoise

--- capsula_tempo/capsula_tempo/settings.py
+++ capsula_tempo/capsula_tempo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,8 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 AUTH_USER_MODEL = 'core.Usuario'
 
 LOGIN_REDIRECT_URL = 'lista'
@@ -127,8 +128,23 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core', 'static'),
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+            if 'test' in sys.argv
+            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        ),
+    },
+}
--- capsula_tempo/core/urls.py
+++ capsula_tempo/core/urls.py
@@ -7,7 +7,7 @@ from core.views import UsuarioPasswordResetCompleteView, HomeView
 from core.views import AutorizarEdicaoView, EditarCapsulaView
 
 urlpatterns = [
-    path('', ListaCapsulas.as_view(), name='lista'),
+    path('lista', ListaCapsulas.as_view(), name='lista'),
     path('deletar/<int:pk>/', DeletarCapsula.as_view(), name='deletar'),
     path('nova/', CriarCapsula.as_view(), name='criar'),
     path('capsula/<int:pk>/', CapsulaDetail.as_view(), name='detalhe'),
@@ -16,6 +16,7 @@ urlpatterns = [
     path('registro/', UsuarioCriaView.as_view(), name='registro'),
     path('perfil/', UsuarioAtualizaView.as_view(), name='perfil'),
     path('home/', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('password_reset/', UsuarioPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', UsuarioPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', UsuarioPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
