60564d80d08d8c6869c3199a917026c57944c1e9
MarcosVieira71 <marcosvieira120404@gmail.com>
2026-04-21 21:41:06 -0300

    teste com gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,9 @@ cd /app/capsula_tempo
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn capsula_tempo.wsgi:application --bind 0.0.0.0:8000
+gunicorn capsula_tempo.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --access-logfile - \
+  --error-logfile - \
+  --capture-output \
+  --enable-stdio-inheritance
